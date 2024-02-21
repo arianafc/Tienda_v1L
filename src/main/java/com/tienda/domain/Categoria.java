9386d45448ba2cfaa318a4f7460182740a3c0ac0
arianafc <arianafc1108@gmail.com>
2024-02-20 22:47:30 -0600

    update
    update

--- src/main/java/com/tienda/domain/Categoria.java
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -4,15 +4,15 @@ import jakarta.persistence.*;
 import java.io.Serializable;
 import lombok.Data;
 
-@Data //es una clase donde se recolectan datos
-@Entity //entidad = tabla
-@Table(name = "categoria") //nombre de la tabla
+@Data
+@Entity
+@Table(name = "categoria")
 
-public class categoria implements Serializable { //serializacion porque se va almacenar ciertos datos en el disco
+public class Categoria implements Serializable { //serializacion porque se va almacenar ciertos datos en el disco
 
-    private static final long serialVersionUID = 1L; //para poder hacer el ciclo de la sumatoria de la categoria. El incremental
+    private static final long serialVersionUID = 1L; //para poder hacer el ciclo de la sumatoria de la categoria.
 
-    @Id //id categoria es la llave de la tabla categoria. Aqui siempre va el primary key
+    @Id //id categoria es la llave de la tabla categoria. 
     @GeneratedValue(strategy = GenerationType.IDENTITY) //Los valores generados que estrategia usan, identico a la BD 
     @Column(name = "id_categoria") //decir cual es el nombre en la base de datos. Se hace la asociación 
     private long idCategoria;
@@ -20,10 +20,10 @@ public class categoria implements Serializable { //serializacion porque se va al
     private String rutaImagen;
     private boolean activo;
 
-    public categoria() {
+    public Categoria() {
     }
 
-    public categoria(String descripcion, boolean activo) {
+    public Categoria(String descripcion, boolean activo) {
         this.descripcion = descripcion;
         this.activo = activo;
     }
